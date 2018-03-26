c9926a498828f58885c638d19fc879860033f9da
ormaks <maks.chytailo@gmail.com>
2018-03-27 01:56:08 +0300

    Redesign google maps Add custom Marker with InfoBox Fix many bugs in TagCanvas.min.js

--- my-app/src/js/Layout.js
+++ my-app/src/js/Layout.js
@@ -1,71 +1,24 @@
 import React from 'react';
 import {Redirect, Route, Switch} from "react-router-dom";
 import {About, Contact, Header, Home, Skills,} from '../js/components';
-/*
-class Loader extends Component {
-	constructor(props) {
-		super(props);
-		this.state = {
-			loadStatus: 0,
-			unloadStatus: 100,
-		};
-	}
 
-	componentWillMount() {
-		for (let i = 0; i <= 100; i++) {
-			setTimeout(() => {
-				this.setState({
-					loadStatus: i
-				});
-			}, 30 * i);
-		}
-	}
-
-	componentWillUnmount() {
-		for (let i = 100; i >= 0; i--) {
-			this.setState({
-				loadStatus: i
-			});
-		}
-	}
-
-	/!*
-		componentWillUnmount() {
-			this.setState({
-				loadStatus: 0
-			})
-		}
-	*!/
-
-	render() {
-		const Comp = this.props.component;
-		const Body = this.state.loadStatus === 100 ?
-			() => (
-				<div style={{left: (100 - this.state.unloadStatus) + "%"}}><Comp/></div>
-			) :
-			() => (<div>Component is loading {this.state.loadStatus}%</div>);
-		return <Body/>;
-	}
-}*/
 
 const prefix = "/react-portfolio";
 
 let Layout = () => (
 	<div>
 		<Header/>
-		<Redirect from={"/"} to={prefix}/>
 		<Route path={prefix}>
 			<Switch>
 				<Redirect from={prefix + "/index.html"} to={prefix + "/"}/>
 				<Route exact path={prefix + "/"} component={Home}/>
-				<Route path={prefix + "/about"} component={About}/>
-				<Route path={prefix + "/skills"} component={Skills}/>
+				<Route exact path={prefix + "/about"} component={About}/>
+				<Route exact path={prefix + "/skills"} component={Skills}/>
 				{/*<Route exact path={prefix + "/works"} component={Works}/>*/}
-				<Route path={prefix + "/contact"} component={Contact}/>
+				<Route exact path={prefix + "/contact"} component={Contact}/>
 			</Switch>
 		</Route>
 	</div>
 );
-
 
 export default Layout;
--- my-app/src/js/components/Contact.js
+++ my-app/src/js/components/Contact.js
@@ -1,283 +1,296 @@
 import React, {Component} from 'react';
-import Map from 'google-map-react';
+import {compose, withProps, withStateHandlers} from "recompose"
+import {GoogleMap, Marker, withGoogleMap, withScriptjs} from "react-google-maps";
+import {InfoBox} from "react-google-maps/lib/components/addons/InfoBox";
+
 import '../../css/contact.css';
+import logo from '../../img/MyMarker.png';
+
+const defaultProps = {
+	center: {lat: 49.8431, lng: 24.02607},
+	zoom: 14,
+	mapStyles: [
+		{
+			"featureType": "all",
+			"elementType": "labels.text.fill",
+			"stylers": [
+				{
+					"color": "#ffffff"
+				}
+			]
+		},
+		{
+			"featureType": "all",
+			"elementType": "labels.text.stroke",
+			"stylers": [
+				{
+					"visibility": "off"
+				}
+			]
+		},
+		{
+			"featureType": "administrative",
+			"elementType": "geometry",
+			"stylers": [
+				{
+					"visibility": "on"
+				},
+				{
+					"color": "#012621"
+				},
+				{
+					"weight": 0.8
+				}
+			]
+		},
+		{
+			"featureType": "administrative.country",
+			"elementType": "labels",
+			"stylers": [
+				{
+					"color": "#012621"
+				}
+			]
+		},
+		{
+			"featureType": "administrative.country",
+			"elementType": "labels.text",
+			"stylers": [
+				{
+					"color": "#0c0000"
+				}
+			]
+		},
+		{
+			"featureType": "administrative.province",
+			"elementType": "labels.text",
+			"stylers": [
+				{
+					"color": "#012621"
+				}
+			]
+		},
+		{
+			"featureType": "administrative.locality",
+			"elementType": "labels.text",
+			"stylers": [
+				{
+					"color": "#012621"
+				}
+			]
+		},
+		{
+			"featureType": "administrative.neighborhood",
+			"elementType": "labels.text",
+			"stylers": [
+				{
+					"color": "#012621"
+				}
+			]
+		},
+		{
+			"featureType": "landscape",
+			"elementType": "geometry",
+			"stylers": [
+				{
+					"color": "#06c5a9"
+				}
+			]
+		},
+		{
+			"featureType": "poi",
+			"elementType": "all",
+			"stylers": [
+				{
+					"color": "#06c5a9"
+				},
+				{
+					"lightness": -7
+				}
+			]
+		},
+		{
+			"featureType": "poi.park",
+			"elementType": "all",
+			"stylers": [
+				{
+					"color": "#06c5a9"
+				}
+			]
+		},
+		{
+			"featureType": "road",
+			"elementType": "geometry.stroke",
+			"stylers": [
+				{
+					"color": "#ffffff"
+				},
+				{
+					"weight": 0.3
+				},
+				{
+					"lightness": 10
+				}
+			]
+		},
+		{
+			"featureType": "road.highway",
+			"elementType": "geometry",
+			"stylers": [
+				{
+					"color": "#047968"
+				},
+				{
+					"lightness": "-7"
+				}
+			]
+		},
+		{
+			"featureType": "road.arterial",
+			"elementType": "geometr	y",
+			"stylers": [
+				{
+					"color": "#06c5a9"
+				},
+				{
+					"visibility": "on"
+				},
+				{
+					"lightness": -15
+				}
+			]
+		},
+		{
+			"featureType": "road.local",
+			"elementType": "geometry",
+			"stylers": [
+				{
+					"color": "#06c5a9"
+				},
+				{
+					"lightness": "7"
+				}
+			]
+		},
+		{
+			"featureType": "transit",
+			"elementType": "geometry",
+			"stylers": [
+				{
+					"color": "#06c5a9"
+				},
+				{
+					"lightness": -34
+				}
+			]
+		},
+		{
+			"featureType": "water",
+			"elementType": "geometry",
+			"stylers": [
+				{
+					"color": "#333739"
+				}
+			]
+		}
+	],
+	divStyle: {
+		color: '#08fdd8',
+		backgroundColor: 'black',
+		margin: '14px',
+		textAlign: 'center',
+		fontSize: '18px',
+		fontWeight: 'bold',
+		opacity: .85,
+		padding: `12px`,
+		width: '300px',
+		transform: 'none',
+		borderRadius: '4px'
+	}
+};
+const StyledMapWithAnInfoBox = compose(
+	withProps({
+		googleMapURL: "https://maps.googleapis.com/maps/api/js?key=AIzaSyBa7trDSsEedbuHDCqBFTLtJIAtSHYrg9s&v=3.exp&libraries=geometry,drawing,places",
+		loadingElement: <div style={{height: `100%`, width: '100%'}}/>,
+		containerElement: <div style={{height: `100%`, width: '100%'}}/>,
+		mapElement: <div style={{height: `100%`, width: '100%'}}/>
+	}),
+	withStateHandlers(() => ({
+		isOpen: false,
+	}), {
+		onToggleOpen: ({isOpen}) => () => ({
+			isOpen: !isOpen,
+		})
+	}),
+	withScriptjs,
+	withGoogleMap
+)(props =>
+	<GoogleMap
+		defaultZoom={defaultProps.zoom}
+		defaultCenter={defaultProps.center}
+		defaultOptions={{styles: defaultProps.mapStyles}}
+	>
+		<Marker
+			position={defaultProps.center}
+			onClick={props.onToggleOpen}
+			icon={{
+				url: logo
+			}}
+		>
+			{props.isOpen &&
+			<InfoBox
+				onCloseClick={props.onToggleOpen}
+				options={{closeBoxURL: ``, enableEventPropagation: true}}
+			>
+				<div style={defaultProps.divStyle}>
+					Heeey!=) <br/> Kredense cafe is my second home!XD <br/>
+					Maybe I'm here at the moment! <br/>
+					So, you are welcome!:)
+				</div>
+			</InfoBox>}
+		</Marker>
+	</GoogleMap>
+);
+
 
 class Contact extends Component {
-	/* constructor(){
-		 super();
-		 let textInput = document.querySelector('input');
-		 let inputWrap = textInput.parentElement ;
-		 var inputWidth = parseInt(getComputedStyle(inputWrap).width);
-		 var svgText = Snap('.line');
-		 var qCurve = inputWidth / 2;  // For correct curving on diff screen sizes
-		 var textPath = svgText.path("M0 0 " + inputWidth + " 0");
-		 var textDown = function(){
-			 textPath.animate({d:"M0 0 Q" + qCurve + " 40 " + inputWidth + " 0"},150,mina.easeout);
-		 };
-		 var textUp = function(){
-			 textPath.animate({d:"M0 0 Q" + qCurve + " -30 " + inputWidth + " 0"},150,mina.easeout);
-		 };
-		 var textSame = function(){
-			 textPath.animate({d:"M0 0 " + inputWidth + " 0"},200,mina.easein);
-		 };
-		 var textRun = function(){
-			 setTimeout(textDown, 200 );
-			 setTimeout(textUp, 400 );
-			 setTimeout(textSame, 600 );
-		 };
-
-		 (function(){
-			 textInput.addEventListener('focus', function(){
-				 var parentDiv = this.parentElement;
-				 parentDiv.classList.add('active');
-				 textRun();
-				 this.addEventListener('blur', function(){
-					 var rg = /^([A-Za-z0-9_\-\.])+\@([A-Za-z0-9_\-\.]{3,9})+\.([A-Za-z]{2,4})$/;
-					 this.value == 0 ? parentDiv.classList.remove('active') : null;
-					 !rg.test(this.value) && this.value != 0 ?
-						 (parentDiv.classList.remove('valid'), parentDiv.classList.add('invalid'), parentDiv.style.transformOrigin="center")
-						 : rg.test(this.value) && this.value != 0 ?
-						 (parentDiv.classList.add('valid'), parentDiv.classList.remove('invalid'), parentDiv.style.transformOrigin="bottom") :null;
-				 });
-				 parentDiv.classList.remove('valid', 'invalid')
-			 });
-		 })();
-	 }
- */
-
-	static defaultProps = {
-		center: {lat: 49.843, lng: 24.025},
-		zoom: 8,
-		mapStyles: [
-			{
-				"featureType": "all",
-				"elementType": "labels.text.fill",
-				"stylers": [
-					{
-						"color": "#ffffff"
-					}
-				]
-			},
-			{
-				"featureType": "all",
-				"elementType": "labels.text.stroke",
-				"stylers": [
-					{
-						"visibility": "off"
-					}
-				]
-			},
-			{
-				"featureType": "administrative",
-				"elementType": "geometry",
-				"stylers": [
-					{
-						"visibility": "on"
-					},
-					{
-						"color": "#012621"
-					},
-					{
-						"weight": 0.8
-					}
-				]
-			},
-			{
-				"featureType": "administrative.country",
-				"elementType": "labels",
-				"stylers": [
-					{
-						"color": "#012621"
-					}
-				]
-			},
-			{
-				"featureType": "administrative.country",
-				"elementType": "labels.text",
-				"stylers": [
-					{
-						"color": "#0c0000"
-					}
-				]
-			},
-			{
-				"featureType": "administrative.province",
-				"elementType": "labels.text",
-				"stylers": [
-					{
-						"color": "#012621"
-					}
-				]
-			},
-			{
-				"featureType": "administrative.locality",
-				"elementType": "labels.text",
-				"stylers": [
-					{
-						"color": "#012621"
-					}
-				]
-			},
-			{
-				"featureType": "administrative.neighborhood",
-				"elementType": "labels.text",
-				"stylers": [
-					{
-						"color": "#012621"
-					}
-				]
-			},
-			{
-				"featureType": "landscape",
-				"elementType": "geometry",
-				"stylers": [
-					{
-						"color": "#06c5a9"
-					}
-				]
-			},
-			{
-				"featureType": "poi",
-				"elementType": "all",
-				"stylers": [
-					{
-						"color": "#06c5a9"
-					},
-					{
-						"lightness": -7
-					}
-				]
-			},
-			{
-				"featureType": "poi.park",
-				"elementType": "all",
-				"stylers": [
-					{
-						"color": "#06c5a9"
-					}
-				]
-			},
-			{
-				"featureType": "road",
-				"elementType": "geometry.stroke",
-				"stylers": [
-					{
-						"color": "#ffffff"
-					},
-					{
-						"weight": 0.3
-					},
-					{
-						"lightness": 10
-					}
-				]
-			},
-			{
-				"featureType": "road.highway",
-				"elementType": "geometry",
-				"stylers": [
-					{
-						"color": "#047968"
-					},
-					{
-						"lightness": "-7"
-					}
-				]
-			},
-			{
-				"featureType": "road.arterial",
-				"elementType": "geometry",
-				"stylers": [
-					{
-						"color": "#06c5a9"
-					},
-					{
-						"visibility": "on"
-					},
-					{
-						"lightness": -15
-					}
-				]
-			},
-			{
-				"featureType": "road.local",
-				"elementType": "geometry",
-				"stylers": [
-					{
-						"color": "#06c5a9"
-					},
-					{
-						"lightness": "7"
-					}
-				]
-			},
-			{
-				"featureType": "transit",
-				"elementType": "geometry",
-				"stylers": [
-					{
-						"color": "#06c5a9"
-					},
-					{
-						"lightness": -34
-					}
-				]
-			},
-			{
-				"featureType": "water",
-				"elementType": "geometry",
-				"stylers": [
-					{
-						"color": "#333739"
-					}
-				]
-			}
-		]
-	};
 
 	render() {
-		return (
-			<div className="contact_content">
-				<span className="tags">&nbsp;&nbsp;&nbsp;&lt;body&gt;</span>
-				<div className="contact_main">
-					<div className="left_side">
-						<span className="tag_h1">&lt;h1&gt;</span> <br/>
-						<span className="text_h1"> Contact </span> <br/>
-						<span className="tag_h1">&lt;h1/&gt;</span> <br/>
-						<form id="contact" autoComplete="off">
-							<div className="input_row">
-								<div className="half">
-									<input placeholder="Name" type="text" name="name"/>
-								</div>
-								<div className="half">
-									<input placeholder="Email" type="email" name="email" required/>
-								</div>
+		return <div className="contact_content">
+			<span className="tags">&nbsp;&nbsp;&nbsp;&lt;body&gt;</span>
+			<div className="contact_main">
+				<div className="left_side">
+					<span className="tag_h1">&lt;h1&gt;</span> <br/>
+					<span className="text_h1"> Contact </span> <br/>
+					<span className="tag_h1">&lt;h1/&gt;</span> <br/>
+					<form id="contact" autoComplete="off">
+						<div className="input_row">
+							<div className="half">
+								<input placeholder="Name" type="text" name="name"/>
 							</div>
-							<div className="input_row">
-								<input className="" placeholder="Subject" type="text" name="subject"/>
-								<label className=""> </label>
+							<div className="half">
+								<input placeholder="Email" type="email" name="email" required/>
 							</div>
-							<div className="input_row">
-								<textarea className="" placeholder="Message" name="msg" required/>
-								<label className=""> </label>
-							</div>
-							<div className="input_submit">
-								<input id="submit" type="submit" className="" value="SEND"/>
-							</div>
-						</form>
-					</div>
-					<div className="right_side">
-						<Map
-							bootstrapURLKeys={{key: ['AIzaSyBa7trDSsEedbuHDCqBFTLtJIAtSHYrg9s']}}
-							center={this.props.center}
-							zoom={this.props.zoom}
-							options={{styles: this.props.mapStyles}}>
-						</Map>
-					</div>
+						</div>
+						<div className="input_row">
+							<input className="" placeholder="Subject" type="text" name="subject"/>
+							<label className=""> </label>
+						</div>
+						<div className="input_row">
+							<textarea className="" placeholder="Message" name="msg" required/>
+							<label className=""> </label>
+						</div>
+						<div className="input_submit">
+							<input id="submit" type="submit" className="" value="SEND"/>
+						</div>
+					</form>
 				</div>
-				<span className="tags">&nbsp;&nbsp;&nbsp;&lt;/body&gt; <br/> &lt;/html&gt;</span>
+				<div className="right_side">
+					<StyledMapWithAnInfoBox/>
+				</div>
 			</div>
-		);
+			<span className="tags">&nbsp;&nbsp;&nbsp;&lt;/body&gt; <br/> &lt;/html&gt;</span>
+		</div>;
 	}
 }
 
